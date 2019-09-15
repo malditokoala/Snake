cfde70f4e4cc05d1801fb1c242bd476968b3f3a6
Edgar Gomez Borboa <edgargomezborboa@hotmail.com>
2019-09-14 20:29:28 -0700

    Initial commit

---
+++ snake.py
@@ -0,0 +1,65 @@
+import turtle
+import time
+
+# Configuracion de la ventana
+wn = turtle.Screen()
+wn.title("Juego de Pong")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+#Cabeza de la serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0, 0)
+cabeza.direction = "stop"
+
+# Funciones
+def arriba():
+    cabeza.direction = "up"
+
+
+def abajo():
+    cabeza.direction = "down"
+
+
+def izquierda():
+    cabeza.direction = "left"
+
+
+def derecha():
+    cabeza.direction = "right"
+
+
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
+
+
+# Teclado
+wn.listen()
+wn.onkeypress(arriba, "Up")
+wn.onkeypress(abajo, "Down")
+wn.onkeypress(izquierda, "Left")
+wn.onkeypress(derecha, "Right")
+
+while True:
+    wn.update()
+    mov()
+    time.sleep(0.1)
+
---
+++ test.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+pluma = turtle.Turtle()
+pluma.speed(1)
+
+
+for i in range(0, 4):
+    pluma.forward(100)
+    pluma.left(90)
+
+
+time.sleep(5.5)
